64c7c3193c1948df2c2ec907800cd79c1161d974
RiccardoSansone <r.sansone@gmail.com>
2023-10-20 23:02:08 +0200

    initial markup structure with Bootstrap

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Animal Shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/style.css">
+</head>
+
+<body>
+
+    <header class="mb-5">
+        <div style="width: 100%;" class="navbar shadow d-flex justify-content-around p-3">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Contacts</a>
+        </div>
+    </header>
+    <!-- /header -->
+
+    <main>
+        <div class="container d-flex justify-content-around wrap gap-3">
+            <div class="col">
+                <div class="card shadow p-3 flex-column gap-2">
+                    <img src="https://picsum.photos/100/100" alt="">
+                    <h4>Lorem</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, illum?</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow p-3 flex-column gap-2">
+                    <img src="https://picsum.photos/100/100" alt="">
+                    <h4>Lorem</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, illum?</p>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card shadow p-3 flex-column gap-2">
+                    <img src="https://picsum.photos/100/100" alt="">
+                    <h4>Lorem</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, illum?</p>
+                </div>
+            </div>
+        </div>  
+    </main>
+    <!-- /main -->
+
+
+    <footer class="mt-5">
+        <div style="width: 100%;" class="p-5 d-flex justify-content-around bg-dark text-light gap-4">
+            <div class="col">
+                <h1 class="text-center">Lorem</h1>
+                <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eveniet autem culpa minus perspiciatis at.</p>
+            </div>
+            <div class="col">
+                <h1 class="text-center">Lorem</h1>
+                <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eveniet autem culpa minus perspiciatis at.</p>
+            </div>
+            <div class="col">
+                <h1 class="text-center">Lorem</h1>
+                <p class="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae eveniet autem culpa minus perspiciatis at.</p>
+            </div>
+        </div>
+    </footer>
+    <!-- /footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
